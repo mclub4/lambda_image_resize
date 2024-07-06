29e902b7afd87d4009f7eff4ac675c146e189745
mclub4 <mclub4@kookmin.ac.kr>
2024-07-06 23:49:00 +0900

    feat: s3에서 이미지 로드해서 메모리에 저장

--- src/main/java/com/lambda/demo/Handler.java
+++ src/main/java/com/lambda/demo/Handler.java
@@ -1,21 +1,65 @@
 package com.lambda.demo;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import org.springframework.beans.factory.annotation.Value;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Handler implements RequestHandler<S3Event, String> {
-    
-    @Value("bucket-name")
-    private final static String UPLOAD_BUCKET_NAME;
-    
 
     @Override
     public String handleRequest(S3Event s3Event, Context context) {
+        LambdaLogger logger = context.getLogger();
+
+        logger.log(s3Event.getRecords().size() + " Images Uploaded Event Accepted \n");
+
+        try{
+            S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().get(0);
+            String srcBucket = record.getS3().getBucket().getName();
+            String srcKey = record.getS3().getObject().getUrlDecodedKey();
+
+            Matcher matcher = Pattern.compile("(.+\\\\/)*(.+)(\\\\..+)$").matcher(srcKey);
+            if (!matcher.matches()) {
+                logger.log("Unable to infer image type for key " + srcKey);
+                return "";
+            }
+
+            String path = matcher.group(1);
+            String fileName = matcher.group(2);
+            String imgType = matcher.group(3);
+
+            final List<String> allowedTypes = List.of(".jpg", "jpeg", "png");
+            if(!allowedTypes.contains(imgType)){
+                logger.log(fileName + " has unsupported image type " + imgType);
+                return "";
+            }
+
+            S3Client s3Client = S3Client.builder().build();
+            InputStream inputStream = s3Client.getObject(GetObjectRequest.builder()
+                    .bucket(srcBucket)
+                    .key(srcKey)
+                    .build());
+
+            BufferedImage srcImage = ImageIO.read(inputStream);
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return "";
     }
 
